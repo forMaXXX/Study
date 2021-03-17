2fbe6a95a09fcbbab24e552740c602efbe33c750
Максим Клоков <maxim2601@mail.ru>
2021-03-17 16:49:20 +0500

    git push

---
+++ MaXXX.HelloWorld/MaXXX.HelloWorld/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace MaXXX.HelloWorld
+{
+    class Program
+    {
+        static readonly int SpeedOfSpelling = 100;
+        static readonly int SpeedOfEnter = 2000;
+        static readonly char Razdelitel = ';';
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            WriteMatrix("Wake up, Neo...;The Matrix has you...;Follow the white rabbit.;Knock, knock, Neo.");
+
+            void WriteMatrix(string str)
+            {
+
+                for (int i = 0; i < str.Length; i++)
+                {                    
+                    if (str[i] == Razdelitel)
+                    {
+                        NewString();
+                    }
+                    else
+                    {
+                        Console.Write(str.ToCharArray(i, 1));
+                        Thread.Sleep(SpeedOfSpelling);
+                    }
+                   
+                }
+
+                NewString();
+            }
+
+            void NewString()
+            {
+                Thread.Sleep(SpeedOfEnter);
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ MaXXX.If/If/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Answer("Введите первое число:");
+            //Console.WriteLine("Число 1: {0}", a);
+            //Console.WriteLine($"Число 1: {a}");
+            int b = Answer("Введите второе число:");
+            int c = Answer("Введите третье число:");
+            int max = a;
+
+            if (b >= a && b >= c)
+            {
+                max = b;
+            }
+            else if (c >= a && c >= b)
+            {
+                max = c;
+            }
+
+            Console.WriteLine("Максимальное из чисел: " + Convert.ToString(max));
+
+            switch (max % 2)
+            {
+                case 0:
+                    Console.WriteLine("Чётное");
+                    break;
+                case 1:
+                    Console.WriteLine("Нечётное");
+                    break;
+            }
+            Console.WriteLine(max < 100 ? "<100" : ">100");
+            Console.ReadLine();
+
+            int Answer (string str) 
+            {
+                Console.WriteLine(str);
+                if (!int.TryParse(Console.ReadLine(), out int answer))
+                {
+                    Console.WriteLine("Ошибка! Только целые числа! (Введённое значение будет считаться как 'ноль')");
+                }
+                return answer;
+            }
+        }
+    }
+}
